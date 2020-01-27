e89dde268ed3c3bfab0af60b3139af87ebca3016
DougBurbidge <dougburbidge@dougburbidge.com>
2020-01-27 19:55:50 +0800

    Make playersBox.LoadGame handle case where no league is open.

--- PlayersBox.cs
+++ PlayersBox.cs
@@ -17,20 +17,23 @@ namespace Torn.UI
 			InitializeComponent();
 			SetSort(0, SortOrder.Ascending);  // Default to sorting by colour then pack.
 		}
-		
+
 		public void LoadGame(League league, ServerGame serverGame)
 		{
 			Items.Clear();
 
-			if (serverGame.Players != null && serverGame.Players.Count == 0 && serverGame.Game != null)  // ServerGame is a fake, created from game; but ServerGame.Players is not filled in yet, so fill it in.
+			if (serverGame.Players.Count == 0 && serverGame.Game != null)  // ServerGame is a fake, created from game; but ServerGame.Players is not filled in yet, so fill it in.
 				foreach (var player in serverGame.Game.Players())
 				{
 					var serverPlayer = new ServerPlayer();
 					player.CopyTo(serverPlayer);
 
-					LeaguePlayer leagueplayer = league.LeaguePlayer(player);
-					if (leagueplayer != null)
-						serverPlayer.Alias = leagueplayer.Name;
+					if (league != null)
+					{
+						LeaguePlayer leagueplayer = league.LeaguePlayer(player);
+						if (leagueplayer != null)
+							serverPlayer.Alias = leagueplayer.Name;
+					}
 
 					serverGame.Players.Add(serverPlayer);
 				}
