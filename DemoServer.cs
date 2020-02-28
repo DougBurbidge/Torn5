c96a7d1098ec12be7787134fe69c9b5e04c36908
DougBurbidge <dougburbidge@dougburbidge.com>
2020-02-28 08:30:24 +0800

    Improve demo server: better fake game start times; deterministic players in each game.

--- DemoServer.cs
+++ DemoServer.cs
@@ -14,8 +14,8 @@ namespace Torn
 
 		public DemoServer()
 		{
-			adjectives = new string[] { "Actual ", "Cyber ", "Dark ", "Laser ", "Mega ", "Phasor ", "Super ", "Ultra ", "Vector ", "Zone " };
-			nouns = new string[] { "Ace", "Blazer", "Chaser", "Dueller", "Rogue", "Runner", "Shark", "Star", "Stunner", "Trekker" };
+			adjectives = new string[] { "Actual ", "Battle ", "Cyber ", "Dark ", "Delta ", "Elite ", "Inter ", "Laser ", "Mega ", "Phasor ", "Super ", "Ultra ", "Vector ", "Zone " };
+			nouns = new string[] { "Ace", "Blaster", "Blazer", "Chaser", "Crystal", "Dueller", "Max", "Rogue", "Runner", "Shark", "Star", "Stunner", "Trekker", "Warrior" };
 		}
 
 		public override List<ServerGame> GetGames()
@@ -27,7 +27,8 @@ namespace Torn
 				ServerGame game = new ServerGame();
 				game.GameId = i;
 				game.Description = "Demo Game";
-				game.Time = DateTime.Now.AddMinutes(i - 10);
+				game.Time = DateTime.Now.AddMinutes(i * 15 - 150);
+				game.EndTime = game.Time.AddMinutes(12);
 				game.OnServer = true;
 				games.Add(game);
 			}
@@ -38,7 +39,7 @@ namespace Torn
 		public override void PopulateGame(ServerGame game)
 		{
 		    game.Players = new List<ServerPlayer>();
-			Random r = new Random();
+			Random r = new Random(game.GameId);
 
 			for (int i = 0; i < 10; i++)
 			{
@@ -46,9 +47,9 @@ namespace Torn
 				player.Colour = (Colour)r.Next(1, 9);
 				player.Score = r.Next(-100, 1000) * 10 + r.Next(0, 3) * 2001;
 				player.Pack = "Pack" + r.Next(1, 30).ToString("D2");
-				var x = r.Next(0, 10);
-				var y = r.Next(0, 10);
-				player.PlayerId = "demo" + (x * 10).ToString() + y.ToString();
+				var x = r.Next(0, adjectives.Length);
+				var y = r.Next(0, nouns.Length);
+				player.PlayerId = string.Format("demo%2d%2d", x * 10, y);
 				player.Alias = adjectives[x] + nouns[y];
 				game.Players.Add(player);
 			}
