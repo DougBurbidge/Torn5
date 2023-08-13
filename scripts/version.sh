d817edeb6d1146ef2fb19f6aa3bf53ebd73df987
BasiliskHill <me@basiliskhill.com>
2023-08-13 18:31:12 +1000

    Feat/torn 6/create torn installer (#35)
    * Creates MVP of installer
    
    Implements an installer that properly places the required .exe and .dll
    into the ProgramFiles (64bit) folder and correctly registers the app
    with its icons.
    
    To ensure the installer reflects the correct versions, the version.sh
    script has been updated to replace the installer package version number.
    
    Addresses TORN-6
    
    * Ensures AppData is properly cleaned on uninstall
    
    Ensures that the Torn folder in the AppData directory is properly
    uninstalled when the application is uninstalled.
    
    Additionally re-arranged and properly comments the Package file
    
    Addresses TORN-6
    
    * Adds GUI to select options
    
    Adds a GUI to allow the user to select the optional shortcuts, change
    the machine install directory, and agree to the Apache license.
    
    Additionally moves the icon resource into the Installer project
    
    Addresses TORN-6
    
    * Add Torn icons to UI
    
    Customises the new UI to utilise Torn icons
    
    Addresses TORN-6
    
    * Ensures installer won't run without NET
    
    Adds a requirement for the NET 4.0 package for the installer to run.
    
    Addresses TORN-6
    
    * Fixes version script
    
    Fixes the version script by replacing the line ends to the correct LF
    ending and ensuring only the first version tag is returned
    
    Addresses TORN-6
    
    * Ensures upgrades work properly with the installer
    
    Moves the version number to the .wixproj to generate a centralised
    variable.
    
    Updates the downgrade error message to indicate the next steps if you
    MUST downgrade.
    
    Addresses TORN-6
    
    * Bump Version
    
    v79.0.1 > v79.1.0
    
    * Fixup compilation
    
    * fixup icon
    
    ---------
    
    Co-authored-by: AJ Horsman <horsmanaj@gmail.com>

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-if [ -z "$1" ] || [ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ]
+if [ $0 != "./scripts/version.sh" ]
+  then
+    echo "Please run this script from the root of the project (above the scripts directory)"
+    exit 1
+elif [ -z "$1" ] || [ "$1" != "major" ] && [ "$1" != "minor" ] && [ "$1" != "patch" ]
   then
     echo "Format should match: ./scripts/version.sh <major | minor | patch>"
     exit 1
 fi
 
+# Installer Paths
+installPackPath="./Torn Installer/Torn Installer.wixproj"
 
 currentVersion=$(<version.txt)  
 
@@ -37,6 +43,14 @@ newVersion="$major.$minor.$patch"
 
 echo $newVersion > version.txt
 
+# Update the version in the Torn Installer
+installerVersion="$(grep -Eom 1 'Torn.Version=[0-9\.]+' "$installPackPath")"
+if [ -z "$installerVersion" ]
+  then
+    echo "WARNING: installer package ($installPackPath) version not updated!"
+  else
+    sed -i "s/$installerVersion/Torn.Version=$newVersion/" "$installPackPath"
+fi
 
 echo $currentVersion \> $newVersion
 
